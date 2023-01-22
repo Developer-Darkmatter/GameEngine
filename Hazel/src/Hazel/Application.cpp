b2923518805cae868f33b476ab1aca2bd5df4eb0
Darkmat13r <developer@darkmatter.in>
2023-01-22 11:17:01 +0530

    Initial Commit

---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,23 @@
+﻿#include "Application.h"
+
+#include <iostream>
+
+namespace Hazel
+{
+    Application::Application()
+    {
+    
+    }
+
+    Application::~Application()
+    {
+    }
+
+    void Application::Run()
+    {
+        while(true)
+        {
+            std::cout << "Application Loop Is running" << std::endl;
+        }
+    }
+}
